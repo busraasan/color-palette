a0538f88c3087d746cb7bbf0856c9a44db3081ee
busraasan <busraasan2@gmail.com>
2023-07-09 17:54:36 +0300

    Change information kept in graph class

--- model/graph.py
+++ model/graph.py
@@ -6,35 +6,25 @@ from utils import *
 from PIL import Image
 
 class DesignGraph():
-    def __init__(self, pretrained_model, idx, data_path):
+    def __init__(self, pretrained_model, all_images, all_bboxes, layers):
         '''
             Node types: Image, Background, Text
             Node features: embedding, color palette, relative size, class
             Edge features: distance between elements
         '''
         self.pretrained_model = pretrained_model
-        self.embedding_list = []
-        self.layers = ['image', 'background', 'text'] # Take this from config file later
-
-        path_idx = "{:04d}".format(idx)
-
-        self.img_path_dict = {
-            'preview': data_path + '/00_preview/' + path_idx + '.png',
-            'background': data_path + '/01_background/' + path_idx + '.png',
-            'image': data_path + '/02_image/' + path_idx + '.png',
-            'text': data_path + '/04_text/' + path_idx + '.png',
+        self.embedding_list = {
+            'image':[], 
+            'background':[], 
+            'text':[]
         }
 
-        self.annotation_path_dict = {
-            'preview': data_path + '/xmls/' +'/00_preview/' + path_idx + '.xml',
-            'background': data_path + '/xmls/' + '/01_background/' + path_idx + '.xml',
-            'image': data_path + '/xmls/' + '/02_image/' + path_idx + '.xml',
-            'text': data_path + '/xmls/' + '/04_text/' + path_idx + '.xml',
-        }
-
-        for i, layer in enumerate(self.layers):
-            img = np.array(Image.open(self.img_path_dict[layer]).convert('RGB'))
-            bboxes = VOC2bbox(self.annotation_path_dict[layer])
+        for layer in layers:
+            for bbox in all_bboxes[layer]:
+                x, y = int(bbox[0][0]), int(bbox[0][1])
+                z, t = int(bbox[2][0]), int(bbox[2][1])
+                cropped_image = all_images[layer][y:t, x:z]
+                self.embedding_list.append(self.pretrained_model(cropped_image))
 
     def calculate_distances(self):
         pass
