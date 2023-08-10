a23ae64e48b9faf431fcea6ee826f124446759fa
busraasan <busraasan2@gmail.com>
2023-08-10 14:53:27 +0300

    Add comments

--- train.py
+++ train.py
@@ -55,6 +55,7 @@ config_file = args.config_file
 with open(config_file, 'r') as f:
     config = yaml.load(f, Loader=yaml.FullLoader)
 
+# Get config
 data_type = config["data_type"]
 model_name = config["model_name"]
 device = config["device"]
@@ -65,6 +66,7 @@ save_path = "../models/" + model_name + "/weights"
 loss_path =  "../models/" + model_name + "/losses"
 log_path = "../models/"+model_name
 
+# If not exists create folders
 if not os.path.exists(save_path):
     os.makedirs(save_path)
 
@@ -74,6 +76,7 @@ if not os.path.exists(loss_path):
 if not os.path.exists(log_path):
     os.makedirs(log_path)
 
+# Init logger
 logging.basicConfig(
     filename=log_path+"/log.log",
     format='%(asctime)s %(levelname)-8s %(message)s',
@@ -84,6 +87,7 @@ logging.getLogger('matplotlib.font_manager').disabled = True
 seed = 42
 logger.info("#" * 100)
 
+# Get training config
 num_epoch = config["num_epoch"]
 batch_size = config["batch_size"]
 feature_size = config["feature_size"]
@@ -92,7 +96,7 @@ weight_decay = config["weight_decay"]
 
 skip_black_flag = True
 
-#model = ColorGNN(feature_size=feature_size).to(device)
+# Chose model based on the config.
 model = model_switch(model_name, feature_size).to(device)
 optimizer = torch.optim.AdamW(model.parameters(), lr=lr, weight_decay=weight_decay)
 criterion = nn.MSELoss()
@@ -111,10 +115,13 @@ def train(data, target_color, node_to_mask):
     optimizer.zero_grad()
     out = model(data.x, data.edge_index.long(), data.edge_weight)
     loss2 = 0
+
+    # This loop is used due to custom CIELab losses.
+    # Mainly MSE is used because having outputs from CIELab is harder.
     mini_batch_size = node_to_mask.shape[0]
     for i in range(mini_batch_size):
         if loss_function == "MSE":
-            loss2 += criterion(out[node_to_mask, :][i], target_color[i])
+            loss2 += criterion(out[node_to_mask, :][i], target_color[i]/255)
         elif loss_function == "CIELab2000":
             loss2 += colormath_CIE2000(out[node_to_mask, :][i], target_color[i])
         elif loss_function == "CIELabCMC":
@@ -128,7 +135,7 @@ def test(data, target_color, node_to_mask):
     model.eval()
     out = model(data.x, data.edge_index.long(), data.edge_weight)
     if loss_function == "MSE":
-        loss2 = criterion(out[node_to_mask, :][0], target_color[0])
+        loss2 = criterion(out[node_to_mask, :][0], target_color[0]/255)
     elif loss_function == "CIELab2000":
         loss2 = colormath_CIE2000(out[node_to_mask, :][0], target_color[0])
     elif loss_function == "CIELabCMC":
@@ -183,6 +190,7 @@ def test_with_node_classification(data, target_color, node_to_mask):
 
 #############################################################################################
 
+# These are for training without using any black samples
 def train_without_black(data, target_color, node_to_mask):
     model.train()
     optimizer.zero_grad()
@@ -247,6 +255,7 @@ for epoch in range(num_epoch):
 
     num_batches = len(train_loader)
     
+    # Run training batches and get losses
     for input_data, target_color, node_to_mask in train_loader:
         loss, out = train(input_data.to(device), target_color.to(device), node_to_mask)
         if loss != None:
@@ -254,6 +263,7 @@ for epoch in range(num_epoch):
         else:
             num_batches-=1
 
+    # Run test batches and get losses
     for input_data, target_color, node_to_mask in test_loader:
         loss, out = test(input_data.to(device), target_color.to(device), node_to_mask)
         if loss != None:
@@ -276,6 +286,7 @@ for epoch in range(num_epoch):
     print("Train loss: ", train_loss)
     print("Test loss: ", val_loss)
 
+    # Save model
     save_plot(train_losses=train_losses, val_losses=val_losses, loss_type="CIE2000", loss_path=loss_path)
     if val_loss < best_loss:
         best_loss = val_loss
@@ -284,4 +295,4 @@ for epoch in range(num_epoch):
         model = model.to(device)
         save_model(state_dict=best_model, train_losses=train_losses, val_losses=val_losses, epoch=epoch, save_path=save_path)
 
-    #lr_scheduler.step()
+    lr_scheduler.step()
